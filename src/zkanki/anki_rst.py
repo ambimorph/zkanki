9812d5fa8988657582e22bf0b41f33ce02f1c026
L. Amber O'Hearn <amber@ambair.local>
2020-07-08 16:04:43 -0600

    Convert reST to AnkiNote

--- src/zkanki/anki_rst.py
+++ src/zkanki/anki_rst.py
@@ -12,6 +12,27 @@ class AnkiNote(dict):
                                      'Extra': 'Something extra'})
         self['tags'] = kwargs.get('tags', ['test'])
 
-        
-                    
+def rst_to_anki_note(rst):
+
+    html = publish_parts(rst, writer_name='html')['html_body']
+    soup = BeautifulSoup(html, 'html.parser')
+
+    textobj = soup.find(id='text')
+    for cloze in textobj.find_all('em'):
+        try:
+            cloze.replace_with('{{'+cloze['class'][0]+'::'+cloze.string+'}}')
+        except KeyError:
+            pass # Not a cloze deletion
+    textobj.h1.decompose()
+    text = ''.join(str(x) for x in textobj.contents)
+
+    extraobj = soup.find(id='extra')
+    extraobj.details.summary.decompose()
+    extra = ''.join(str(x) for x in extraobj.details.contents)
+
+    footer = soup.find(class_='footer')
+    tagtup = [f for f in footer.stripped_strings]
+    tags = tagtup[1].split()
+
+    return AnkiNote(fields = {'Text': text, 'Extra': extra}, tags=tags)
                 
